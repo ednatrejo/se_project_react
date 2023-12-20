f7724a1ec7884e45e08df1670a263360752a6de0
ednatrejo <Trejo.edna1379@gmail.com>
2023-12-20 16:03:34 -0600

    update image errors

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,63 +1,63 @@
 const weatherOptions = [
   {
-    url: require("../images/Day/sunny-day.svg").default,
+    url: require("../../images/Day/sunny-day.svg").default,
     day: true,
-    type: "sunny",
+    type: "sunny-day",
   },
   {
-    url: require("../images/Day/cloudy-day.svg").default,
+    url: require("../../images/Day/cloudy-day.svg").default,
     day: true,
-    type: "cloudy",
+    type: "cloudy-day",
   },
   {
-    url: require("../images/Day/fog-day.svg").default,
+    url: require("../../images/Day/fog-day.svg").default,
     day: true,
-    type: "foggy",
+    type: "foggy-day",
   },
   {
-    url: require("../images/Day/rain-day.svg").default,
+    url: require("../../images/Day/rain-day.svg").default,
     day: true,
-    type: "rainy",
+    type: "rainy-day",
   },
   {
-    url: require("../images/Day/snow-day.svg").default,
+    url: require("../../images/Day/snow-day.svg").default,
     day: true,
-    type: "snowy",
+    type: "snowy-day",
   },
   {
-    url: require("../images/Day/storm-day.svg").default,
+    url: require("../../images/Day/storm-day.svg").default,
     day: true,
     type: "stormy",
   },
   {
-    url: require("../images/Night/cloudy-night.svg").default,
+    url: require("../../images/Night/cloudy-night.svg").default,
     day: false,
     type: "cloudy",
   },
   {
-    url: require("../images/Night/fog-night.svg").default,
+    url: require("../../images/Night/fog-night.svg").default,
     day: false,
     type: "foggy",
   },
   {
-    url: require("../images/Night/rain-night.svg").default,
+    url: require("../../images/Night/rain-night.svg").default,
     day: false,
     type: "rainy",
   },
   {
-    url: require("../images/Night/snow-night.svg").default,
+    url: require("../../images/Night/snow-night.svg").default,
     day: false,
     type: "snowy",
   },
   {
-    url: require("../images/Night/storm-night.svg").default,
+    url: require("../../images/Night/storm-night.svg").default,
     day: false,
     type: "stormy",
   },
 ];
 
 const WeatherCard = ({ day, type, weatherTemp = 0 }) => {
-  const imageSrc = weatherConditions.filter((i) => {
+  const imageSrc = weatherOptions.filter((i) => {
     return i.day === day && i.type === type;
   });
 
