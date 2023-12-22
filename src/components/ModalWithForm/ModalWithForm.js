1c8a89c8c1d23b0f488f123aa6e1e9ccbfeb7e23
ednatrejo <Trejo.edna1379@gmail.com>
2023-12-21 20:13:33 -0600

    fixing Modal With Form

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,23 +1,41 @@
+import { useEffect, useRef } from "react";
 import "./ModalWithForm.css";
 
 const ModalWithForm = ({
-  children,
-  buttonText = "Add garment",
   title,
+  children,
   onClose,
   name,
+  isOpen,
+  onSubmit,
 }) => {
-  console.log("ModalWithForm");
+  const ref = useRef();
+
+  const handleOutsideClick = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      onClose();
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
   return (
-    <div className={`modal modal_type_${name}`}>
-      <div className="modal__content">
-        <button type="button" onClick={onClose}>
-          Close
-        </button>
-        <h3>{title}</h3>
-        <form>
+    <div className={`modal modal_type_${name}`} onClick={handleOutsideClick}>
+      <div className="modal__content" ref={ref}>
+        <button
+          className="modal__close-button"
+          type="button"
+          onClick={onClose}
+        ></button>
+        <h3 className="modal__title">{title}</h3>
+        <form onSubmit={handleSubmit}>
           {children}
-          <button type="submit">{buttonText}</button>
+          <button className="modal__button" type="submit">
+            Add garment
+          </button>
         </form>
       </div>
     </div>
