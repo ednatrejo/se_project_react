9a8ab593b9706324df7120333a5208fe98ad5854
ednatrejo <Trejo.edna1379@gmail.com>
2024-03-07 18:20:13 -0600

    update info

--- src/components/EditProfileModal/EditProfileModal.js
+++ src/components/EditProfileModal/EditProfileModal.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import React, { useContext } from "react";
 import { useForm } from "../../hooks/useForm";
@@ -8,70 +6,15 @@ const EditProfileModal = ({ onClose, isOpen, onSubmit, isLoading }) => {
   const { currentUser } = useContext(CurrentUserContext);
   const { values, handleChange, setValues } = useForm({});
 
-  // const [name, setName] = useState("");
-  // const handleEditName = (e) => {
-  //   setName(e.target.value);
-  // };
-
-  // const [avatar, setAvatar] = useState("");
-  // const handleEditAvatar = (e) => {
-  //   setAvatar(e.target.value);
-  // };
-
-  // useEffect(() => {
-  //   setName(currentUser.name ?? "");
-  //   setAvatar(currentUser.avatar ?? "");
-  // }, [isOpen]);
-
-  useEffect(() => {
-    setValues({
-      name: currentUser.name ?? "",
-      avatar: currentUser.avatar ?? "",
-    });
-  }, [isOpen]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(values);
+  const [name, setName] = useState("");
+  const handleEditName = (e) => {
+    setName(e.target.value);
   };
 
-  return (
-    <ModalWithForm
-      title="Change profile data"
-      onClose={onClose}
-      isOpen={isOpen}
-      onSubmit={handleSubmit}
-    >
-      <label>
-        Name*
-        <input
-          className="modal__input"
-          type="name"
-          name="name"
-          minLength="2"
-          maxLength="30"
-          placeholder="New Name"
-          value={values.name || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Avatar*
-        <input
-          className="modal__input"
-          type="url"
-          name="avatar"
-          id="avatar"
-          placeholder="NewAvatar URL"
-          value={values.avatar || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <button className="modal__button" type="submit">
-        {isLoading ? "Saving..." : "Save changes"}
-      </button>
-    </ModalWithForm>
-  );
+  const [avatar, setAvatar] = useState("");
+  const handleEditAvatar = (e) => {
+    setAvatar(e.target.value);
+  };
 };
 
 export default EditProfileModal;
